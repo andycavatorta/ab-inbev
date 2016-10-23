8b7d0b1de971a9cb3ae5585fcf78057af7565ef4
gianordoli <gianordoligabriel@yahoo.com.br>
2016-10-23 11:23:11 -0400

    updated test

--- classifier_usage.py
+++ classifier_usage.py
@@ -1,5 +1,5 @@
 from classifier import Classifier
 
 image_classifier = Classifier()
-guess = image_classifier.guess_image("image_classifier/test_images/test_019.jpg")
+guess = image_classifier.guess_image("image_classifier/test_images/test_011.jpg")
 print(guess)
