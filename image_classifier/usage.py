19295e1758bb3680fc44341d8e209bcf0f36bc14
gianordoli <gianordoligabriel@yahoo.com.br>
2016-10-23 10:57:18 -0400

    converted to module

--- image_classifier/usage.py
+++ image_classifier/usage.py
@@ -1,1 +1,5 @@
-from image_classifier import Classifier
+from classifier import Classifier
+
+image_classifier = Classifier()
+guess = image_classifier.guess_image("test_images/test_019.jpg")
+print(guess)
