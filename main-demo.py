cb6e1685c5ed500f0b7876ae9c86d58e8acad76e
Andy Cavatorta <andycavatorta@gmail.com>
2017-01-19 15:54:51 -0500

    fixing data structure parser

--- main-demo.py
+++ main-demo.py
@@ -229,22 +229,23 @@ class Classifier():
             graph_def.ParseFromString(f.read())
             _ = tf.import_graph_def(graph_def, name='')
         with tf.Session() as sess:
-            for imageMetadata in imageMetadataList:
-    
-                image_data = tf.gfile.FastGFile(imageMetadata["pathName"], 'rb').read()
-                
-                softmax_tensor = sess.graph.get_tensor_by_name('final_result:0')# Feed the image_data as input to the graph and get first prediction
-                predictions = sess.run(softmax_tensor, \
-                         {'DecodeJpeg/contents:0': image_data})
-                top_k = predictions[0].argsort()[-len(predictions[0]):][::-1]# Sort to show labels of first prediction in order of confidence
-                print "top_k=", repr(top_k)
-                for node_id in top_k:
-                    human_string = self.label_lines[node_id]
-                    score = predictions[0][node_id]
-                    print('%s (score = %.5f)' % (human_string, score))
-                # print(self.label_lines[top_k[0]])
-                results.append(self.label_lines[top_k[0]])
-            return results
+            for camera in  imageMetadataList:
+                for imageMetadata in camera:
+        
+                    image_data = tf.gfile.FastGFile(imageMetadata["pathName"], 'rb').read()
+                    
+                    softmax_tensor = sess.graph.get_tensor_by_name('final_result:0')# Feed the image_data as input to the graph and get first prediction
+                    predictions = sess.run(softmax_tensor, \
+                             {'DecodeJpeg/contents:0': image_data})
+                    top_k = predictions[0].argsort()[-len(predictions[0]):][::-1]# Sort to show labels of first prediction in order of confidence
+                    print "top_k=", repr(top_k)
+                    for node_id in top_k:
+                        human_string = self.label_lines[node_id]
+                        score = predictions[0][node_id]
+                        print('%s (score = %.5f)' % (human_string, score))
+                    # print(self.label_lines[top_k[0]])
+                    results.append(self.label_lines[top_k[0]])
+                return results
 
 
     def guess_images(self, foldername):
