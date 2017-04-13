b46272cbc2f543a713444e8c81a2dff6602d4e3a
Nick Joliat <njoliat@mit.edu>
2017-04-13 15:25:15 -0400

    more helper script stuff

--- cans-bottles-cases/2017-04-05-18-04-01/collation_helper.py
+++ cans-bottles-cases/2017-04-05-18-04-01/collation_helper.py
@@ -1,6 +1,8 @@
+from PIL import Image
 import os
 import shutil
 
+# move beers from "image_N/beer_type/" dir struct to "beer_type" dir struct, renaming to avoid collisions.
 def collate_beers(beer_type):
     type_idx = 0
     for img_dir in ["image_1", "image_2", "image_3"]:
@@ -8,3 +10,9 @@ def collate_beers(beer_type):
         for can_photo_name in os.listdir(src_loc):
             shutil.copyfile(src_loc + "/" + can_photo_name, "%s/i%02d.png" % (beer_type, type_idx))
             type_idx += 1
+
+# just png to jpg conversion
+def convert_img(beer_type):
+    for png_file in os.listdir(beer_type):
+        im = Image.open(beer_type + "/" + png_file)
+        im.save(beer_type + "/" + png_file[:-3] + "jpg")
