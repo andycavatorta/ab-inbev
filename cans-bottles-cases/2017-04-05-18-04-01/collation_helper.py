1206a927fcdbdf12c4766fedf2cc764249cc5cfe
Nick Joliat <njoliat@mit.edu>
2017-05-02 18:18:13 -0400

    small generalization in collation script

--- cans-bottles-cases/2017-04-05-18-04-01/collation_helper.py
+++ cans-bottles-cases/2017-04-05-18-04-01/collation_helper.py
@@ -3,9 +3,9 @@ import os
 import shutil
 
 # move beers from "image_N/beer_type/" dir struct to "beer_type" dir struct, renaming to avoid collisions.
-def collate_beers(beer_type):
+def collate_beers(beer_type, img_dirs):
     type_idx = 0
-    for img_dir in ["image_1", "image_2", "image_3"]:
+    for img_dir in img_dirs:
         src_loc = img_dir + "/" + beer_type
         for can_photo_name in os.listdir(src_loc):
             shutil.copyfile(src_loc + "/" + can_photo_name, "%s/i%02d.png" % (beer_type, type_idx))
