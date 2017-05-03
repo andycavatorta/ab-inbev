d774c6743bced910e4155cd4473a9afea8b5b5bc
Nick Joliat <njoliat@mit.edu>
2017-05-03 12:31:19 -0400

    classifier: update paths, start returning more data

--- classifier_cans.py
+++ classifier_cans.py
@@ -8,18 +8,18 @@ class Classifier():
 
 		# Loads label file, strips off carriage return
 		self.label_lines = [line.rstrip() for line 
-						   in tf.gfile.GFile("image_classifier_cans/tf_files/retrained_labels.txt")]
+						   in tf.gfile.GFile("image_classifier_all/tf_files/retrained_labels_cans.txt")]
 
 
-	def guess_image(self, test_images_dir="image_classifier_cans/test_images"):
+	def guess_image(self, test_images_dir="image_classifier_all/test_images_cans"):
 	
 		files = sorted([f for f in os.listdir(test_images_dir) if f.endswith(".jpg")])
 		results = []
 
-		print("Found " + str(len(files)) + " files")
+		#print("Found " + str(len(files)) + " files")
 
 		# Unpersists graph from file
-		with tf.gfile.FastGFile("image_classifier_cans/tf_files/retrained_graph.pb", 'rb') as f:
+		with tf.gfile.FastGFile("image_classifier_all/tf_files/retrained_graph_cans.pb", 'rb') as f:
 			graph_def = tf.GraphDef()
 			graph_def.ParseFromString(f.read())
 			_ = tf.import_graph_def(graph_def, name='')
@@ -30,7 +30,7 @@ class Classifier():
 
 				# Read in the image_data
 				image_data = tf.gfile.FastGFile(test_images_dir + "/" + image, 'rb').read()
-				print(test_images_dir + "/" + image)
+				#print(test_images_dir + "/" + image)
 
 				# Feed the image_data as input to the graph and get first prediction
 				softmax_tensor = sess.graph.get_tensor_by_name('final_result:0')
@@ -41,12 +41,14 @@ class Classifier():
 				# Sort to show labels of first prediction in order of confidence
 				top_k = predictions[0].argsort()[-len(predictions[0]):][::-1]
 
+                                scores = [] # list of (name, confidence)
 				for node_id in top_k:
-					human_string = self.label_lines[node_id]
+					#human_string = self.label_lines[node_id]
 					score = predictions[0][node_id]
-					print('%s (score = %.5f)' % (human_string, score))
+                                        #scores += (human_string, score)
+                                        scores += score
+                                scores = [predictions[0][node_id] for node_id in top_k]
 				
-				# print(self.label_lines[top_k[0]])
-				results.append(self.label_lines[top_k[0]])
+				results.append((self.label_lines[top_k[0]], scores))
 				
 			return results
