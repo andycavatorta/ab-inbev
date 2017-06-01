f20c0c25d15eafa0aceff4abd48506f02f73baf8
Nick Joliat <njoliat@mit.edu>
2017-06-01 16:25:25 -0400

    more cmd line args for batch cropping script etc

--- cropping_code/batch_cropping_stuff.py
+++ cropping_code/batch_cropping_stuff.py
@@ -5,7 +5,7 @@ import sys
 import CropBC
 import UnWrapImage
 
-def unwarp_images_dir(in_dir, out_dir):
+def unwarp_images(in_dir, out_dir):
     if os.path.isdir(in_dir):
         files = os.listdir(in_dir)
         img_files = [f for f in files if f.endswith("png")]
@@ -13,13 +13,11 @@ def unwarp_images_dir(in_dir, out_dir):
             img = cv2.imread(os.path.join(in_dir, img_file), cv2.IMREAD_COLOR)
             unwarped = UnWrapImage.undistort_image(image=img)
             cv2.imwrite(os.path.join(out_dir, img_file), unwarped)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
 
 def crop_images(in_dir, out_dir, tmp_dir):
     # min and max r vals just avg of junwei's original hardcoded vals
-    minr = 74
-    maxr = 96
+    minr = 65
+    maxr = 100
     if os.path.isdir(out_dir):
         print("out already exists; maybe use a different one so you don't overwrite stuff?")
         return
@@ -36,8 +34,22 @@ def crop_images(in_dir, out_dir, tmp_dir):
         for img_file in img_files:
             CropBC.crop(minr, maxr, in_dir, img_file, out_dir, tmp_dir)
 
+def print_usage():
+    print('usage: "%s unwarp <in_dir> <out_dir>" or "%s crop <in_dir> <out_dir> <tmp_dir>"' % (sys.argv[0], sys.argv[0]))
+
 if __name__== '__main__':
-    if len(sys.argv) == 4:
-        crop_images(sys.argv[1], sys.argv[2], sys.argv[3])
+    if len(sys.argv) >= 2:
+        if sys.argv[1] == "unwarp":
+            if len(sys.argv) == 4:
+                unwarp_images(sys.argv[2], sys.argv[3])
+            else:
+                print_usage()
+        elif sys.argv[1] == "crop":
+            if len(sys.argv) == 5:
+                crop_images(sys.argv[2], sys.argv[3], sys.argv[4])
+            else:
+                print_usage()
+        else:
+            print_usage()
     else:
-        print('usage: "%s <in_dir> <out_dir> <tmp_dir>"')
+        print_usage()
